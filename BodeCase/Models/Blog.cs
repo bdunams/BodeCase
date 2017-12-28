58c393e9dd0697a594f3c897ee408a8dcabe42e9
bdunams <dabadestboy1@gmail.com>
2017-12-28 14:10:17 -0500

    Created Blog model and added BlogModel migration.

---
+++ BodeCase/Models/Blog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BodeCase.Models
+{
+    public class Blog : DbContext
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string BlogTitle { get; set; }
+
+        [Required]
+        public string BlogContent { get; set; }
+
+        [Required]
+        public DateTime DateWritten { get; set; }
+
+    }
+}
--- BodeCase/Models/IdentityModels.cs
+++ BodeCase/Models/IdentityModels.cs
@@ -20,6 +20,8 @@ namespace BodeCase.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Blog> Blogs { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
